92f828a49909fc7aedd5fab724b9445fc5bb2655
Zach Bergson <zach.bergson@gmail.com>
2015-11-22 11:47:35 -0500

    added user ref in review model

--- models/review.js
+++ models/review.js
@@ -3,6 +3,7 @@ var mongoose = require('mongoose');
 var reviewSchema = new mongoose.Schema({
   stars: Number,
   content: String,
+  user_id: Number,
   created_at: { type: Date, default: Date.now },
   updated_at: { type: Date, default: Date.now }
 });
